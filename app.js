b2018b207f7b3a8f061ba179d0dbde820d16cf31
soheb <shaikhsoheb7905@gmail.com>
2022-01-18 11:46:43 +0530

    Fixed Procfile

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ require('dotenv').config();
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended:false}));
